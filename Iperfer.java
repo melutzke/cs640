bbdf677172721394f98240c7a811da1a104cd760
Tyler A Stank <tbear2500@gmail.com>
2015-02-03 17:58:53 -0600

    FUCK!!!

--- Iperfer.java
+++ Iperfer.java
@@ -7,7 +7,12 @@
 //
 
 
+
 public class Iperfer {
+
+    
+
+
     static void parse_args(String[] args, IperferConfig config) throws Exception {
         String argument_number_error = "Error: missing or additional arguments";
         String argument_format_error = "Error: incorrect format of arguments";
@@ -28,39 +33,39 @@ public class Iperfer {
                 //TODO: the repeated code here should be condensed
                 switch (args[1]) {
                     case "-h":
-                        config.set_host(args[2]);
+                        set_host(args[2]);
                     break;
                     case "-p":
-                        config.set_port(Integer.parseInt(args[2]));
+                        set_port(Integer.parseInt(args[2]));
                     break;
                     case "-t":
-                        config.set_time(Integer.parseInt(args[2]));
+                        set_time(Integer.parseInt(args[2]));
                     break;
                     default:
                         throw new Exception(argument_format_error);
                 }
                 switch (args[3]) {
                     case "-h":
-                        config.set_host(args[4]);
+                        set_host(args[4]);
                     break;
                     case "-p":
-                        config.set_port(Integer.parseInt(args[4]));
+                        set_port(Integer.parseInt(args[4]));
                     break;
                     case "-t":
-                        config.set_time(Integer.parseInt(args[4]));
+                        set_time(Integer.parseInt(args[4]));
                     break;
                     default:
                         throw new Exception(argument_format_error);
                 }
                 switch (args[5]) {
                     case "-h":
-                        config.set_host(args[6]);
+                        set_host(args[6]);
                     break;
                     case "-p":
-                        config.set_port(Integer.parseInt(args[6]));
+                        set_port(Integer.parseInt(args[6]));
                     break;
                     case "-t":
-                        config.set_time(Integer.parseInt(args[6]));
+                        set_time(Integer.parseInt(args[6]));
                     break;
                     default:
                         throw new Exception(argument_format_error);
